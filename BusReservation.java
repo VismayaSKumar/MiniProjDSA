fe7283530f7a22c78ff833e0e10f551e9945d498
VismayaSKumar <vismayaskumar10@gmail.com>
2024-01-19 13:21:20 +0530

    edit1

--- BusReservation.java
+++ BusReservation.java
@@ -1,7 +1,11 @@
+
+
 public class BusReservation{
     public static void main(String[] args) {
-        System.out.println("Hello/n Welcome to Bus Reservation System");
+        System.out.println("__________________________________________");
+        System.out.println("    Welcome to Bus Reservation System   ");
         System.out.println("------------------------------------------");
+        System.out.println("");
     }
 }
 
